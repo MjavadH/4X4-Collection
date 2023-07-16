d0f22aa969a1b3fe9e7055bfc6410569814aff11
M_javad Hacker <m.hacker0110@gmail.com>
2023-07-16 09:59:49 +0330

    Add Page 404 Night sky

---
+++ Fantasy/Night sky/assets/script.js
@@ -0,0 +1,59 @@
+// Move the background by changing the mouse position
+let container = document.getElementById('particles-js');
+window.onmousemove = function (e) {
+    let x = -e.x / 5,
+        y = -e.y / 5;
+    container.style.right = x + 'px';
+    container.style.bottom = y + 'px';
+}
+/*Particle script*/
+particlesJS('particles-js', {
+    'particles': {
+        'number': {
+            'value': 25,
+            'density': {
+                'enable': true,
+                'value_area': 300
+            }
+        },
+        'color': {value: ['#01ffbb', '#d72323', '#fff'] /*Stars color*/},
+        'shape': {
+            'type': 'star',
+        },
+        'opacity': {
+            'value': 0.7,
+            'random': true,
+        },
+        'size': {
+            'value': 1,
+            'random': true,
+        },
+        'line_linked': {
+            'enable': false,
+        },
+    },
+    'interactivity': {
+        'detect_on': 'canvas',
+        'events': {
+            'onhover': {
+                'enable': true,
+                'mode': 'bubble'
+            },
+            'onclick': {
+                'enable': true,
+                'mode': 'push'
+            },
+            'resize': true
+        },
+        modes: {
+            bubble: {
+                distance: 50,
+                size: 2,
+                duration: 2,
+                opacity: 2,
+                speed: 3
+            },
+        }
+    },
+    'retina_detect': true
+});
